6ef67b6afcb3e43dfdcb02080203395451732c39
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-02-24 11:19:38 +0000

    previousNumberImplemented

--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -9,17 +9,32 @@ function Questions({questions,results,keepResults,isFinished}) {
             
    //state for current question
   const [currentQuestion, setCurrentQuestion] = useState(0);
+  const [hideButton, setHideButton] = useState(false);
  
   
   let answers = questions[currentQuestion].answers;
   
   const lastQuestion = currentQuestion === questions.length- 1 ? "find out results" : "Next Question";
 
+
+  function previousQuestion() {
+    setCurrentQuestion(previous => {
+      let questionNumber = previous - 1;
+      if (questionNumber === 0) {
+        setHideButton(true);
+      }
+    
+      return questionNumber;
+    
+    })
+
+
+  }
   
 
-  //changes number of question
+  //changes number of question to move forward
   function questionNumber() {
-    
+    setHideButton(false);
     setCurrentQuestion(previous => {
       let questionNumber = previous + 1;
       if (questionNumber >= questions.length) {
@@ -40,11 +55,13 @@ function Questions({questions,results,keepResults,isFinished}) {
             <li onClick={(e) => keepResults(i, currentQuestion)} className={results[currentQuestion] === i ? "selected" : "option"} key={i}>{answer}
             </li>)}
         </ol>
-        <ProgressBar questionNumber={currentQuestion}/>
-        <button onClick={questionNumber}>{lastQuestion}</button>
+      
+        <ProgressBar questionNumber={currentQuestion} />
         
-       
-  
+        <button className={hideButton ? "hideMe" : "buttonsStyle"} onClick={previousQuestion}>Previous question</button>
+
+        <button 
+          onClick={questionNumber}>{lastQuestion}</button>
         </>
 
 )
