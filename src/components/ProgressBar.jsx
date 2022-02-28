eb1d95f6a40b1bbb281e6884b7fafd444bd2f589
Natalie Acevedo <natalieacevedo25@gmail.com>
2022-02-28 11:05:29 +0000

    fixed prettier problems

--- src/components/ProgressBar.jsx
+++ src/components/ProgressBar.jsx
@@ -1,26 +1,16 @@
 import React from "react";
 
-function ProgressBar({questionNumber}) {
-    
-    const max = 5;
+function ProgressBar({ questionNumber }) {
+  const max = 5;
 
-    let answersLength = questionNumber +1;
-    
+  let answersLength = questionNumber + 1;
 
-   
-
-   
-
-    return (
-        <>
-
-            <progress value={answersLength} max={max}></progress>
-            <span>{` ${answersLength} out of ${max} questions`}</span>
-        </>
-    )
-
-
-
-};
+  return (
+    <>
+      <progress value={answersLength} max={max}></progress>
+      <span>{` ${answersLength} out of ${max} questions`}</span>
+    </>
+  );
+}
 
 export default ProgressBar;
